0ac2425cd358d82f6dcadd6444d65be635de6490
korri damodharrao <damodhardhamu56@gmail.com>
2022-09-24 23:20:19 +0530

    new file added

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,9 @@
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 0; i <= 10; i++) {
+            sum = sum + i;
+        }
+        System.out.println(sum);
+    }
+}
